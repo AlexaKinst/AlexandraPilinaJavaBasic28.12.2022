8f58ec1d64c1c512fc85f40bf2e17b3536ee9295
User <alexandra.pilina97@gmail.com>
2022-12-28 23:44:50 +0200

    Second commit

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,5 +1,6 @@
 public class HomeWork {
     public static void main(String[] args) {
-        System.out.println("That`s my fist project");
+
+        System.out.println("That`s my second project");
     }
 }
